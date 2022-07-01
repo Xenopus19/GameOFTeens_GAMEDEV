e556514efacfb244d7010be4761edaba1f988eab
GmaB-a <vladik11012006@gmail.com>
2022-07-01 20:50:36 +0300

    script korobki

---
+++ Assets/Scripts/Objects/BoxScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour
+{
+    public int scoreAddAmount;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") Score.instance.AddScore(scoreAddAmount);
+    }
+}
