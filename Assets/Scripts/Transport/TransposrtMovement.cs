68a71799228243566559af6b22ac66aa4cd58f26
AlexandraKorniichuk <65713096+AlexandraKorniichuk@users.noreply.github.com>
2022-07-01 21:31:21 +0300

    Transport movement

--- Assets/Scripts/Transport/TransposrtMovement.cs
+++ Assets/Scripts/Transport/TransposrtMovement.cs
@@ -2,13 +2,25 @@ using UnityEngine;
 
 public class TransposrtMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float m_MovementSpeed = 0.1f;
 
     void Update()
     {
+        Drive();
+    }
+
+    private void Drive()
+    {
+        Vector3 direction = CalculateDirection();
+        transform.Translate(direction * m_MovementSpeed * Time.deltaTime);
+    }
+
+    private Vector3 CalculateDirection()
+    {
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
         
+        return new Vector3(mousePos3D.x - transform.position.x, 0, 0);
     }
 }
