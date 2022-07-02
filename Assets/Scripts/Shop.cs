c6ccb867c83cca5d8fa8eff6d78326fd8ced57a8
Denis <denispirog878@gmail.com>
2022-07-02 18:40:23 +0300

    Shop

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -15,11 +15,24 @@ public class Shop : MonoBehaviour
         currentIndex = SavingTransport.TransportIndex;
         buttons[currentIndex].gameObject.SetActive(true);
         moneyText.text = Money.LoadMoney().ToString();
+       
     }
 
     private void Update()
     {
         moneyText.text = Money.LoadMoney().ToString();
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Money.IncreaseMoney(100000);
+        }
+
+        for (int i = 0; i < shopItems.Length; i++)
+        {
+            if (shopItems[i].isBought)
+            {
+                buttons[i].gameObject.SetActive(false);
+            }
+        }
     }
 
     public void Next()
@@ -27,19 +40,27 @@ public class Shop : MonoBehaviour
         if (currentIndex + 1 >= shopItems.Length) return;
 
         CloseAll();
+
         shopItems[currentIndex + 1].gameObject.SetActive(true);
-        buttons[currentIndex + 1].gameObject.SetActive(true);
-        currentIndex += 1;
+
+        if(!shopItems[currentIndex + 1].isBought) buttons[currentIndex + 1].gameObject.SetActive(true);
+
+        if (shopItems[currentIndex + 1].isBought) SavingTransport.SaveTransport(currentIndex + 1);
+        currentIndex += 1;    
     }
 
     public void Previous()
     {
         if (currentIndex - 1 < 0) return;
-
         CloseAll();
+
         shopItems[currentIndex - 1].gameObject.SetActive(true);
-        buttons[currentIndex - 1].gameObject.SetActive(true);
-        currentIndex -= 1;
+
+        if (!shopItems[currentIndex - 1].isBought) buttons[currentIndex - 1].gameObject.SetActive(true);
+
+        if (shopItems[currentIndex - 1].isBought) SavingTransport.SaveTransport(currentIndex - 1);
+
+        currentIndex -= 1;  
     }
 
     private void CloseAll()
@@ -53,5 +74,11 @@ public class Shop : MonoBehaviour
         {
             button.gameObject.SetActive(false);
         }
+    }
+
+    private void OnDisable()
+    {
+        CloseAll();
+        shopItems[PlayerPrefs.GetInt("Transport")].gameObject.SetActive(true);
     }
 }
